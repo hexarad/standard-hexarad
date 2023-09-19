5d75debd3712d395ef36d28d08218d6979a8ce5d
B Martin <bmartin@hexarad.com>
2023-09-19 14:03:42 +0100

    bundle gem standard-hexarad

---
+++ lib/standard/hexarad.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "hexarad/version"
+
+module Standard
+  module Hexarad
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/standard/hexarad_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Standard::Hexarad do
+  it "has a version number" do
+    expect(Standard::Hexarad::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
